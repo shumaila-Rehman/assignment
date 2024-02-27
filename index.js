9134f5c77c63ca0c06482830aa2e537e637e8b1b
Shumaila-Rehman <pathan369khan@gmail.com>
2024-02-27 21:41:57 +0500

    add my files

---
+++ index.js
@@ -0,0 +1,27 @@
+// let message : string = "Shumaila";
+// console.log(message);
+// message = "123";
+// console.log(message);
+// // concatination
+// let personName :string = "Bilal";
+// console.log("Good day, " + personName + "!");
+//E.g Person's age is 35 => number
+//  let finalage : number = personAge + "5";
+//  console.log("person's age is " + finalage);
+//string, number, boolean,
+// Example : 1 string.
+var Name = "Aehmad";
+console.log("Good day, " + Name + "!");
+//Example : 2 Numbern.
+var PersonAge = 30;
+var finalAge = PersonAge + 5;
+console.log("Person's age is " + finalAge);
+//Example : 3 Boolean.
+var isSub = true;
+console.log(isSub);
+//Example .4 Null
+var Value = null;
+console.log(Value);
+//Example .5 undefined 
+var randomValue = undefined;
+console.log(randomValue);
---
+++ index.ts
@@ -0,0 +1,49 @@
+// let message : string = "Shumaila";
+// console.log(message);
+// message = "123";
+// console.log(message);
+
+// // concatination
+// let personName :string = "Bilal";
+// console.log("Good day, " + personName + "!");
+
+//E.g Person's age is 35 => number
+//  let finalage : number = personAge + "5";
+//  console.log("person's age is " + finalage);
+ //string, number, boolean,
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // Example : 1 string.
+ let Name : string = "Aehmad";
+ console.log("Good day, " + Name +"!") ;
+
+ //Example : 2 Numbern.
+ let PersonAge : number = 30;
+ let finalAge :number = PersonAge + 5;
+ console.log("Person's age is "+ finalAge );
+
+ //Example : 3 Boolean.
+ let isSub : boolean= true;
+ console.log(isSub);
+
+
+ //Example .4 Null
+ let Value: null=null ;
+ console.log(Value);
+
+ //Example .5 undefined 
+ let randomValue:undefined = undefined;
+ console.log(randomValue);
+
